8eb856e8225f4255cc75b43dc1894efab7c5c9d5
defrell01 <sashka.sahkov@gmail.com>
2022-09-04 16:24:25 +0700

    1

--- back/server.js
+++ back/server.js
@@ -3,10 +3,18 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
+/ * Config */
+import { connectDB } from './config/db.js'
+
+/ * MiddleWare * /
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 / *Routes */
 import userRoutes from '../routes/userRoutes.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -16,6 +24,9 @@ if (process.env.NODE_ENV === 'development')
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
+
+app.use(errorHandler)
+app.use(notFound)
 
 const PORT = process.env.PORT || 5000
 
