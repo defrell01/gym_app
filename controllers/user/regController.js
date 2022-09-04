87648ad232e002d1a15ee61a4aa71e8c3088a31c
defrell01 <sashka.sahkov@gmail.com>
2022-09-04 16:31:08 +0700

    2nd commit

--- controllers/user/regController.js
+++ controllers/user/regController.js
@@ -1,5 +1,6 @@
 import User from '../../models/userModel.js'
 import asyncHandler from 'express-async-handler'
+import { generateToken } from '../../back/helpers/generateToken.js'
 
 //  @desc Register user
 //  @route POST /api/users/
@@ -19,7 +20,7 @@ export const registerUser = asyncHandler(async (req, res) => {
         email, password
     })
 
-    //Create token
+    const token = generateToken(user._id)
 
-    res.json(user)
+    res.json({user, token})
 })
